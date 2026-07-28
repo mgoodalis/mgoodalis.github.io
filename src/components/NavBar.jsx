1b11e1c9e289028f25123b20beb825a0b5232270
Matt Goodalis <goodalis@wisc.edu>
2026-07-27 20:30:52 -0500

    Trying to fix the Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,30 +4,30 @@ import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
-      <Container>
-        <Navbar.Brand as={Link} to="/">
-          My Portfolio
+    <Navbar bg="light" expand="lg" fixed="top" className="shadow-sm w-100">
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/" className="fw-semibold text-dark me-4">
+          Matt Goodalis Photography
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/" className="text-dark">
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/about">
+            <Nav.Link as={Link} to="/about" className="text-dark">
               About Me
             </Nav.Link>
-            <Nav.Link as={Link} to="/portfolio">
+            <Nav.Link as={Link} to="/portfolio" className="text-dark">
               Portfolio
             </Nav.Link>
-            <Nav.Link as={Link} to="/prints">
+            <Nav.Link as={Link} to="/prints" className="text-dark">
               Prints
             </Nav.Link>
-            <Nav.Link as={Link} to="/equipment">
+            <Nav.Link as={Link} to="/equipment" className="text-dark">
               Equipment
             </Nav.Link>
-            <Nav.Link as={Link} to="/contact">
+            <Nav.Link as={Link} to="/contact" className="text-dark">
               Contact
             </Nav.Link>
           </Nav>
